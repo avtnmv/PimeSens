1988369d9e45382be28c291dc57167dd6f13a46c
Yaroslav Avtonomov <avtnmv@icloud.com>
2024-08-12 20:58:33 +0300

    feat: add media queries

--- app.js
+++ app.js
@@ -186,3 +186,83 @@ arrows.addEventListener('click', function() {
 
   slide();
 });
+
+function look() {
+  var param = document.getElementById('div1');
+  var svgIcon = document.querySelector('.svg-icon'); 
+
+  
+  if (param.style.display == "none") {
+      param.style.display = "block";
+      svgIcon.classList.add('rotated'); 
+  } else {
+      param.style.display = "none";
+      svgIcon.classList.remove('rotated'); 
+  }
+}
+
+
+function look2() {
+  var param = document.getElementById('div2');
+  var svgIcon2 = document.querySelector('.svg-icon2'); 
+
+  
+  if (param.style.display == "none") {
+      param.style.display = "block";
+      svgIcon2.classList.add('rotated'); 
+  } else {
+      param.style.display = "none";
+      svgIcon2.classList.remove('rotated'); 
+  }
+}
+
+function look3() {
+  var param = document.getElementById('div3');
+  var svgIcon3 = document.querySelector('.svg-icon3'); 
+
+  
+  if (param.style.display == "none") {
+      param.style.display = "block";
+      svgIcon3.classList.add('rotated'); 
+  } else {
+      param.style.display = "none";
+      svgIcon3.classList.remove('rotated'); 
+  }
+}
+function look4() {
+  var param = document.getElementById('div4');
+  var svgIcon4 = document.querySelector('.svg-icon4'); 
+
+  
+  if (param.style.display == "none") {
+      param.style.display = "block";
+      svgIcon4.classList.add('rotated'); 
+  } else {
+      param.style.display = "none";
+      svgIcon4.classList.remove('rotated'); 
+  }
+}
+
+
+function look5() {
+  var param = document.getElementById('div5');
+  var svgIcon5 = document.querySelector('.svg-icon5'); 
+
+  
+  if (param.style.display == "none") {
+      param.style.display = "block";
+      svgIcon5.classList.add('rotated'); 
+  } else {
+      param.style.display = "none";
+      svgIcon5.classList.remove('rotated'); 
+  }
+}
+
+const burgerMenu = document.getElementById("burger-menu");
+const burgerIcon = document.getElementById("burger-icon");
+
+burgerIcon.addEventListener('click', () => {
+  burgerMenu.classList.toggle('show');
+  burgerIcon.classList.toggle('active');
+  document.body.classList.toggle('no-scroll', burgerMenu.classList.contains('show'));
+});
