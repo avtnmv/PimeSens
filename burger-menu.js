2c24885eb19a464b22f85cd5c289ff06a0c062bc
Yaroslav Avtonomov <avtnmv@icloud.com>
2024-08-21 17:57:23 +0300

    feat: update

--- burger-menu.js
+++ burger-menu.js
@@ -6,3 +6,13 @@ burgerIcon.addEventListener('click', () => {
   burgerIcon.classList.toggle('active');
   document.body.classList.toggle('no-scroll', burgerMenu.classList.contains('show'));
 });
+
+const burgerLinks = document.querySelectorAll('.burger-menu a');
+
+burgerLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    burgerMenu.classList.remove('show');
+    burgerIcon.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+  });
+});
